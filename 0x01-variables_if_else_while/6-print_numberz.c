a7491ac8955172fb1364991b34dac2587bc1cb54
sajedaAl <sajedaaldorash@gmail.com>
2022-10-21 15:28:26 -0700

    mm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print_alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
