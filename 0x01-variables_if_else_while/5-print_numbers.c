2f71f7224b2a9e28c42cac9490304a4b9c300bce
sajedaAl <sajedaaldorash@gmail.com>
2022-10-21 14:46:53 -0700

    ndd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print_alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
