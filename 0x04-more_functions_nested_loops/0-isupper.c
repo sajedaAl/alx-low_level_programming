e8d2210ce91c2a82e77bfdedc83c107f9b639b39
sajedaAl <sajedaaldorash@gmail.com>
2022-10-30 12:25:54 -0700

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return (1) if its upper and return (0) if not 
+ */
+int _isupper(int c)
+{
+	if (c >= 65)
+	{
+		if (c <= 90)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef _main_h_
+#define _main_h_
+#include <stdio.h>
+int _isupper(int c);
+#endif /**_main_h_*/
