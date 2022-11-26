a3ac4c9513cbadd26bb18f59fa6ce27b895612ba
sajedaAl <sajedaaldorash@gmail.com>
2022-11-26 11:29:42 -0800

    mm

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line n times.
+ * @n: the number of times
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
