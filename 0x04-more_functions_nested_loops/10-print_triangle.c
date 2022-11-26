74e2bb818a0760f5edbb280a6560099ddf520db2
sajedaAl <sajedaaldorash@gmail.com>
2022-11-26 14:07:03 -0800

    hh

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - print straight line n times.
+ * @size: the number of times
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
+		for (j--; j <size; j++)
+		{
+			_putchar(35);
+		}
+		if (i < (size - 1))
+				_putchar('\n');
+	}
+	_putchar('\n');
+}
